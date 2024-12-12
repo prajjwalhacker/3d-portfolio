f7b0bac0e56f2cbd465694763883fd29b83a6f58
prajjwalsoni123 <prajjwalsoni123@gmail.com>
2024-12-12 11:40:02 +0530

    bounce class addition

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -26,10 +26,10 @@ const Hero = () => {
   return (
     <section className="min-h-screen flex flex-col w-full relative">
        <div className="w-full mx-auto flex flex-col sm:mt-36 mt-20 c-space gap-3">
-         <p className="sm:text-3xl text-xl font-medium text-white text-center font-generalsans">
+         <p className="sm:text-3xl text-xl font-medium text-white text-center font-generalsans bouncing-text">
            Hi, I am Prajjwal Soni <span className="waving-hand">👋</span>
          </p>
-         <p className="hero_tag text-gray_gradient">
+         <p className="hero_tag text-gray_gradient bouncing-text">
             Senior FullStack Engineer
          </p>
         </div>
